f1191237301453f613d1ada51f62cc4f233e6751
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-20 17:10:52 -0300

    creating class skeleton and adding objects into the list

--- src/Challenge/Title.java
+++ src/Challenge/Title.java
@@ -1,5 +1,30 @@
 package Challenge;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Title {
+public class Title implements Comparable {
+    //variable
+    String nome;
 
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        //objects
+        var horse = new Title();
+        var cat = new Title();
+        var bee = new Title();
+
+        //list
+        List<Title> objectsList = new ArrayList<>();
+
+        //adding on list
+        objectsList.add(horse);
+        objectsList.add(cat);
+        objectsList.add(bee);
+
+
+    }
 }
