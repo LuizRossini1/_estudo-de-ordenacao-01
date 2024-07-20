31144922685351f958b5f99d06b0967bf5c125d3
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-20 16:58:13 -0300

    creating class skeleton

--- src/Challenge/Main.java
+++ src/Challenge/Main.java
@@ -13,7 +13,25 @@ Adicione elementos e imprima a lista, mostrando que é possível trocar facilmen
 5 - Modifique o Exercício 4 para declarar a variável de lista como a interface List,
 demonstrando o uso de polimorfismo. */
 package Challenge;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
+        //instantiating a list
+        List<Integer> numberList = new ArrayList<>();
 
+        //variables
+        int numero1 = 1;
+        int numero2 = 5;
+        int numero3 = 10;
+
+        //adding on list
+        numberList.add(numero1);
+        numberList.add(numero2);
+        numberList.add(numero3);
+
+        //printing list
+        System.out.println(numberList);
+    }
 }
