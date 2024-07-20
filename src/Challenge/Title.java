a17ad02ce93ad3ee824665b42c5f49e2f9a5a0b5
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-20 17:43:24 -0300

    implementing LinkedList

--- src/Challenge/Title.java
+++ src/Challenge/Title.java
@@ -1,6 +1,7 @@
 package Challenge;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Title implements Comparable<Title> {
@@ -27,25 +28,46 @@ public class Title implements Comparable<Title> {
         var ant = new Title("ant");
         var cat = new Title("cat");
         var bee = new Title("bee");
+        //more objects
+        var chicken = new Title("Chicken");
+        var dog = new Title("Dog");
+        var worm = new Title("Worm");
 
-        //list
+        //lists
         List<Title> objectsList = new ArrayList<>();
+        List<Title> otherList = new LinkedList<>();
 
         //adding on list
         objectsList.add(ant);
         objectsList.add(cat);
         objectsList.add(bee);
+        //adding on another list
+        otherList.add(chicken);
+        otherList.add(worm);
+        otherList.add(dog);
 
-        //loop to printing each object
+        //loop to printing each object without order
         for (Title title : objectsList) {
-            System.out.println(title.getNome());
+            System.out.println("List without order: " +title.getNome());
         }
+
         //calling method
         Collections.sort(objectsList);
-
-        //loop to printing each object
+        //loop to printing each object with order
         for (Title title : objectsList) {
-            System.out.println(title.getNome());
+            System.out.println("List with order: " +title.getNome());
+        }
+
+        //loop to printing each object of another list without order
+        for (Title title : otherList) {
+            System.out.println("List without order: " +title.getNome());
+        }
+
+        //calling method of another list
+        Collections.sort(otherList);
+        //loop to printing each object of another list with order
+        for (Title title : otherList) {
+            System.out.println("List with order: " +title.getNome());
         }
     }
 }
