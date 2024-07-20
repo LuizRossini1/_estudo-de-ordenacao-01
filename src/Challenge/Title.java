4a790539a9ab0d60af7db2fe475b8908dc3fc47e
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-20 17:14:36 -0300

    transforming variable into private and creating getter

--- src/Challenge/Title.java
+++ src/Challenge/Title.java
@@ -1,10 +1,16 @@
 package Challenge;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Title implements Comparable {
     //variable
-    String nome;
+    private String nome;
+
+    //gettter
+    public String getNome() {
+        return nome;
+    }
 
     @Override
     public int compareTo(Object o) {
@@ -25,6 +31,9 @@ public class Title implements Comparable {
         objectsList.add(cat);
         objectsList.add(bee);
 
+        System.out.println(objectsList);
+        Collections.sort(objectsList);
+        System.out.println(objectsList);
 
     }
 }
